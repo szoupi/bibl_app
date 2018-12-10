9f870db5dc08dae42850fe038ae77080f20f370a
szoupi <szoupi@gmail.com>
2018-12-10 20:47:08 +0200

    set el_GR locale

--- bible/forms.py
+++ bible/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django import forms
 from django.core.exceptions import ValidationError
 from .models import Book, Chapter, Verse, Annotation
+from django.utils.translation import gettext as _
 
 
 class BookForm(forms.ModelForm):
@@ -54,6 +55,7 @@ class UserLoginForm(forms.ModelForm):
 class ContactForm(forms.Form):
     # gmail overrides from_email option with EMAIL_HOST_USER
     from_email = forms.EmailField(required=False)
+    # self.fields['from_email'].label = _(FROM_EMAIL)
     subject = forms.CharField(required=True)
     name = forms.CharField(required=True)
     email = forms.EmailField(required=True)

9a8d2262f875115f7cb1f5f95fd41defccfc2fbd
szoupi <szoupi@gmail.com>
2018-12-23 16:33:59 +0200

    add link i forgot my password

--- bible/urls.py
+++ bible/urls.py
@@ -30,8 +30,11 @@ urlpatterns = [
         template_name='password_change.html', success_url='/accounts/password_change_done')),
     url(r'^password_change_done/',
         auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html')),
-    url(r'password_reset/$', auth_views.PasswordResetView.as_view(template_name='password_reset.html', email_template_name='password_reset_email.html',
-                                                                  subject_template_name='password_reset_subject.txt', success_url='/accounts/password_reset_done/', from_email='szoupi@gmail.com')),
+    url(r'password_reset/$', auth_views.PasswordResetView.as_view(template_name='password_reset.html',
+        email_template_name='password_reset_email.html', 
+        subject_template_name='password_reset_subject.txt',
+        success_url='/accounts/password_reset_done/',
+        from_email='szoupi@gmail.com'), name='password_reset'),
     url(r'password_reset_done/',
         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html')),
     url(r'password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(
